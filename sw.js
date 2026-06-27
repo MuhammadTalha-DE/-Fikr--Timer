6e8a49000d34f73b6dc0b803725b0531c018aa6a
Muhammad Talha <muhammadtalha1730117301@gmail.com>
2026-06-27 13:29:43 +0500

    Refactor service worker for new asset management
    Updated cache name and asset list for service worker.

--- sw.js
+++ sw.js
@@ -1,28 +1,22 @@
-const CACHE_NAME = 'fikr-timer-v1';
+const CACHE_NAME = 'fikr-pro-v1';
 const ASSETS = [
-  './',
-  './index.html',       // or your HTML file name
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+    './',
+    './index.html',
+    './css/style.css',
+    './js/config.js',
+    './js/app.js',
+    './js/timer.js',
+    './js/breathing.js',
+    './js/analytics.js',
+    './js/storage.js',
+    './manifest.json',
+    './icons/icon-192.png',
+    './icons/icon-512.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
